a863a979af255f45ac6ed71777d2640fbdcf0d35
Lukas Frei <lukas.frei@zuehlke.com>
2022-08-08 22:03:53 +0200

    Make bootstrap server configurable

--- uc-iot/sensor/src/main/java/com/zuehlke/training/kafka/iot/sensor/config/ProducerConfiguration.java
+++ uc-iot/sensor/src/main/java/com/zuehlke/training/kafka/iot/sensor/config/ProducerConfiguration.java
@@ -14,7 +14,6 @@ public class ProducerConfiguration {
     @Bean
     public Map<String, Object> producerConfigs() {
         Map<String, Object> props = new HashMap<>();
-        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
         props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         return props;
