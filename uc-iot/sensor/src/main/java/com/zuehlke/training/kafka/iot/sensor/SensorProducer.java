70ee0298e8c3f9b7a9f858499f75ba7173bd876d
Lukas Frei <lukas.frei@zuehlke.com>
2022-08-09 20:58:39 +0200

    Send sensor measurements in avro and connect to schema registry

--- uc-iot/sensor/src/main/java/com/zuehlke/training/kafka/iot/sensor/SensorProducer.java
+++ uc-iot/sensor/src/main/java/com/zuehlke/training/kafka/iot/sensor/SensorProducer.java
@@ -1,5 +1,6 @@
 package com.zuehlke.training.kafka.iot.sensor;
 
+import com.zuehlke.training.kafka.iot.SensorMeasurement;
 import com.zuehlke.training.kafka.iot.sensor.config.SensorConfig;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -15,11 +16,11 @@ import java.util.concurrent.ThreadLocalRandom;
 public class SensorProducer {
 
     private final SensorConfig sensorConfig;
-    private final KafkaTemplate<String, String> kafkaTemplate;
+    private final KafkaTemplate<String, SensorMeasurement> kafkaTemplate;
 
     @Scheduled(fixedDelay = 10000)
     public void sendMeasurement() {
-        String measurement = getMeasurement();
+        SensorMeasurement measurement = getMeasurement();
 
         log.info("Sending measurement of {} for sensor {} to topic {}",
                 measurement,
@@ -34,10 +35,14 @@ public class SensorProducer {
         );
     }
 
-    private String getMeasurement() {
+    private SensorMeasurement getMeasurement() {
+        return new SensorMeasurement(sensorConfig.getSensorId(), System.currentTimeMillis(), getMeasurementValue());
+    }
+
+    private Long getMeasurementValue() {
         Long min = sensorConfig.getMinValue();
         Long max = sensorConfig.getMaxValue();
 
-        return String.valueOf(ThreadLocalRandom.current().nextLong(max - min) + min);
+        return ThreadLocalRandom.current().nextLong(max - min) + min;
     }
 }
